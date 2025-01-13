ac10514ed482260093751f2f627f9617edacf693
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2025-01-12 19:38:49 -0500

    Fix security issues

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -95,6 +96,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+    ReadHeaderTimeout: time.Second * 10,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
